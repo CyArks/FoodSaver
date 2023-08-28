e093a4cd3aa351b1486e347cdbf117a4765c0c0b
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-28 14:51:38 +0200

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def has_role(self, role):
+        return self.role == role
+
+    @classmethod
+    def find_by_id(cls, user_id):
+        return cls.query.filter_by(id=user_id).first()
+
+# pip install Flask-SQLAlchemy Werkzeug
+# Initialize this somewhere in your application
+# db.init_app(app)
