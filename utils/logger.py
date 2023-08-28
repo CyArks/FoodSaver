b5a5a8a26485fa7d71bdd23fe057f3c2068d1c67
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-28 15:09:39 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(app):
+    # Setup basic logging configuration
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+    )
+
+    # Integrate with Flask
+    logging.getLogger('werkzeug').setLevel(logging.ERROR)
+    app.logger.handlers.extend(logging.getLogger("gunicorn").handlers)
