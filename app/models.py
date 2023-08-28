5cd5431df66a62379f8b3a93963912b5aadb93d9
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-28 15:34:24 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(50), nullable=False, default='user')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    fridge = db.relationship('Fridge', backref='owner', lazy='dynamic')
+    dietary_preferences = db.relationship('DietaryPreferences', backref='owner', lazy='dynamic')
+    ratings = db.relationship('Ratings', backref='rater', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -27,6 +30,26 @@ class User(db.Model):
     def find_by_id(cls, user_id):
         return cls.query.filter_by(id=user_id).first()
 
-# pip install Flask-SQLAlchemy Werkzeug
-# Initialize this somewhere in your application
-# db.init_app(app)
+class Fridge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(64), index=True)
+    item_weight = db.Column(db.Float)
+    expiration_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class DietaryPreferences(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    preference = db.Column(db.String(64), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(128), index=True)
+    ingredients = db.Column(db.String(256))
+    ratings = db.relationship('Ratings', backref='recipe', lazy='dynamic')
+
+class Ratings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
