73f4da9ba982b831cf4ac71ca6d6ae87b4962d53
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-29 10:24:04 +0200

    Update cache_manager.py

--- utils/cache_manager.py
+++ utils/cache_manager.py
@@ -1,5 +1,7 @@
 from cachetools import TTLCache, cached
+import logging
 
+logger = logging.getLogger(__name__)  # Added for logging
 # Setup cache with 1000 item limit and 3 minutes time-to-live (TTL)
 offers_cache = TTLCache(maxsize=1000, ttl=180)
 
@@ -17,8 +19,6 @@ def get_offer(offer_id):
     }
 
 def invalidate_offer_cache(offer_id):
-    """
-    Function to invalidate a cached offer.
-    """
     if offer_id in offers_cache:
         del offers_cache[offer_id]
+        logger.info(f"Cache invalidated for offer_id: {offer_id}")
