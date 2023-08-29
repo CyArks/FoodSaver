d20e9955008c749ca5ac23492db1d644f156b2a0
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-29 09:27:50 +0200

    Create GroceryListComponent.js

---
+++ frontend/src/components/GroceryListComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GroceryListComponent = ({ items }) => {
+  return (
+    <div>
+      {/* Render your grocery list */}
+      {/* Use the 'items' prop to display the list of grocery items */}
+    </div>
+  );
+};
+
+export default GroceryListComponent;
