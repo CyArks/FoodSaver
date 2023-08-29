678a601ff9647503fde12ad3b82c6d66db9c551b
Florian Manhartseder <98718854+CyArks@users.noreply.github.com>
2023-08-29 09:27:35 +0200

    Create MealPlanComponent.js

---
+++ frontend/src/components/MealPlanComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealPlanComponent = ({ recipes }) => {
+  return (
+    <div>
+      {/* Render your meal plans */}
+      {/* Use the 'recipes' prop to display the selected recipes */}
+    </div>
+  );
+};
+
+export default MealPlanComponent;
